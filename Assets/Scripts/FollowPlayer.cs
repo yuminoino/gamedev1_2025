795a348774eb918a4943087772d80d51eff1c5bb
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-10-10 13:52:25 +0200

    FollowPlayer

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,10 +7,10 @@ public class FollowPlayer : MonoBehaviour
     {
         
     }
-
+    public GameObject player;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position;
     }
 }
