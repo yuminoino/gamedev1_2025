3d1cb43ea850f1fbf80329eab993c6e405046f1e
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-10-10 13:26:13 +0200

    Time20

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 40);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
