948235f90e3f07fcf7ce6166ecff765b79d2c90a
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-10-10 13:47:54 +0200

    FollowPlayer

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-
+public class FollowPlayer : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-    public float speed = 5.0f;
+
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
     }
 }
