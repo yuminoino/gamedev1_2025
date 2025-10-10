fc7ee2bc0f962ca69cb0ab57db412e4b7c30a239
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-10-10 13:24:56 +0200

    RigidBody

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 40);
+    }
+}
