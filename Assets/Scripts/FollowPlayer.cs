3c7703cc43064e3ee4f8ec449546a9cb2142be67
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-10-17 10:11:38 +0200

    Update FollowPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,9 +8,10 @@ public class FollowPlayer : MonoBehaviour
         
     }
     public GameObject player;
+    public Vector3 offset = new Vector3(0, 5, -7);
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + offset ;
     }
 }
